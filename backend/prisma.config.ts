3e7efd103b7cd65b0788a09b8382091c38bff330
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2026-02-03 11:58:41 +0800

    config: 修改prisma直连源

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,11 +1,15 @@
-import 'dotenv/config'
+import dotenv from "dotenv";
 import { defineConfig, env } from "prisma/config";
+
+dotenv.config({ path: ".env" });
+dotenv.config({ path: ".env.local", override: true });
+
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: { 
     path: 'prisma/migrations',
   },
   datasource: { 
-    url: env("DATABASE_URL") 
+    url: env("DIRECT_URL") 
   }
 });
