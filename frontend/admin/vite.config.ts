35ffc79f20ab525901527f89301e6186d02e6340
xiaoluobouu <15395544126@163.com>
2026-02-25 16:29:38 +0800

    chore: optimize build config and fix format

--- frontend/admin/vite.config.ts
+++ frontend/admin/vite.config.ts
@@ -9,7 +9,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const isAnalyze = mode === 'analyze'
   console.log('mode: ', mode)
   return defineConfig({
     plugins: [
